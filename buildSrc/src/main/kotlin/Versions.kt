ec7f56abf2cafbc326cd629ffe716bf9d8ff24d8
Kazusa1412 <36687391+Kazusa1412@users.noreply.github.com>
2021-09-23 04:56:35 +0800

    versions

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,19 @@
+/*
+ * Use of this source code is governed by the GNU AFFERO GENERAL PUBLIC LICENSE Version 3.
+ *
+ * https://github.com/Kazusa1412/yeshin/blob/main/LICENSE
+ *
+ */
+
+
+@Suppress("Unused")
+object Versions {
+
+    const val kotlin = "1.5.31"
+    const val serialization = "1.3.0-RC"
+    const val coroutines = "1.5.2"
+    const val atomifu = "0.16.3"
+    const val ktor = "1.6.3"
+    const val kotlinxHtml = "0.7.2"
+    const val kotlinxNodejs = "0.0.7"
+}
