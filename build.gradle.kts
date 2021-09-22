f0653df51778e80d6d35a39330397c7e07e81ba6
Kazusa1412 <36687391+Kazusa1412@users.noreply.github.com>
2021-09-23 04:38:50 +0800

    init gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+/*
+ * Use of this source code is governed by the GNU AFFERO GENERAL PUBLIC LICENSE Version 3.
+ *
+ * https://github.com/Kazusa1412/yeshin/blob/main/LICENSE
+ *
+ */
+
+group = "com.elouyi"
+version = "0.0.1"
+
